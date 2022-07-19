6b6e9d1f861a6ad87f7584b1d113db82556f2505
Darynka <darynakuzmenko45@gmail.com>
2022-07-19 13:27:52 +0300

    social-network

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.logodesign.net/logo/line-art-house-with-leaves-5828ld.png'
+                 alt="Something beautiful"></img>
+        </header>
+    );
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://get-edu.kz/wp-content/uploads/2020/04/helpbox-contact.jpg' alt="Main-img"></img>
+            </div>
+            <div>
+                ava+discription
+            </div>
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    );
+}
